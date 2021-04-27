6ffb9eef722a064b784a61734d1496c617c1419b
Jesse Feng <fxm0016@126.com>
2021-04-27 21:15:49 +0800

    Update Navbar.vue

--- packages/vuepress-theme-purple/components/Navbar.vue
+++ packages/vuepress-theme-purple/components/Navbar.vue
@@ -1,92 +1,5 @@
-<template>
-  <header class="navbar">
-    <div class="navbar-container">
-      <SidebarButton @toggle-sidebar="$emit('toggle-sidebar')" />
-
-      <router-link :to="$localePath" class="home-link">
-        <img
-          class="logo"
-          v-if="$site.themeConfig.logo"
-          :src="$withBase($site.themeConfig.logo)"
-          :alt="$siteTitle"
-        />
-        <span
-          ref="siteName"
-          class="site-name"
-          v-if="$siteTitle"
-          :class="{ 'can-hide': $site.themeConfig.logo }"
-        >{{ $siteTitle }}</span>
-      </router-link>
-
-      <div
-        class="links"
-        :style="linksWrapMaxWidth ? {
-        'max-width': linksWrapMaxWidth + 'px'
-      } : {}"
-      >
-        <AlgoliaSearchBox v-if="isAlgoliaSearch" :options="algolia" />
-        <SearchBox
-          v-else-if="$site.themeConfig.search !== false && $page.frontmatter.search !== false"
-        />
-        <NavLinks class="can-hide" />
-      </div>
-    </div>
-  </header>
-</template>
-
 <script>
-import AlgoliaSearchBox from '@AlgoliaSearchBox';
-import SearchBox from '@SearchBox';
-import SidebarButton from '@parent-theme/components/SidebarButton.vue';
-import NavLinks from '@parent-theme/components/NavLinks.vue';
-
-export default {
-  components: { SidebarButton, NavLinks, SearchBox, AlgoliaSearchBox },
-
-  data() {
-    return {
-      linksWrapMaxWidth: null
-    };
-  },
-
-  mounted() {
-    const MOBILE_DESKTOP_BREAKPOINT = 719; // refer to config.styl
-    const NAVBAR_VERTICAL_PADDING =
-      parseInt(css(this.$el, 'paddingLeft')) +
-      parseInt(css(this.$el, 'paddingRight'));
-    const handleLinksWrapWidth = () => {
-      if (document.documentElement.clientWidth < MOBILE_DESKTOP_BREAKPOINT) {
-        this.linksWrapMaxWidth = null;
-      } else {
-        this.linksWrapMaxWidth =
-          this.$el.offsetWidth -
-          NAVBAR_VERTICAL_PADDING -
-          ((this.$refs.siteName && this.$refs.siteName.offsetWidth) || 0);
-      }
-    };
-    handleLinksWrapWidth();
-    window.addEventListener('resize', handleLinksWrapWidth, false);
-  },
-
-  computed: {
-    algolia() {
-      return (
-        this.$themeLocaleConfig.algolia || this.$site.themeConfig.algolia || {}
-      );
-    },
-
-    isAlgoliaSearch() {
-      return this.algolia && this.algolia.apiKey && this.algolia.indexName;
-    }
-  }
-};
-
-function css(el, property) {
-  // NOTE: Known bug, will return 'auto' if style value is 'auto'
-  const win = el.ownerDocument.defaultView;
-  // null means not to return pseudo styles
-  return win.getComputedStyle(el, null)[property];
-}
+export { default } from '@parent-theme/components/Navbar.vue';
 </script>
 
 <style lang="stylus">
@@ -94,36 +7,19 @@ function css(el, property) {
   padding $navbarVerticalPadding $navbarHorizontalPadding
   line-height $navbarHeight - $navbarVerticalPadding * 2
 
-  a, span, img
-    display inline-block
-
   .logo
     height $navbarHeight - $navbarVerticalPadding * 2
     min-width $navbarHeight - $navbarVerticalPadding * 2
-    margin-right 0.8rem
-    vertical-align top
 
   .site-name
-    font-size 1.3rem
-    font-weight 600
     color $siteNameColor
-    position relative
 
   .links
-    padding-left 1.5rem
-    box-sizing border-box
     background-color $primaryBgColor
-    white-space nowrap
-    font-size 0.9rem
-    position absolute
     right $navbarHorizontalPadding
     top $navbarVerticalPadding
-    display flex
 
     .search-box
-      flex 0 0 auto
-      vertical-align top
-
       input
         background-color $searchboxBgColor
         border-color $searchboxBorderColor
@@ -131,20 +27,4 @@ function css(el, property) {
 
         &:focus
           border-color $searchboxBorderAccentColor
-
-@media (max-width: $MQMobile)
-  .navbar
-    padding-left 4rem
-
-    .can-hide
-      display none
-
-    .links
-      padding-left 1.5rem
-
-    .site-name
-      width calc(100vw - 9.4rem)
-      overflow hidden
-      white-space nowrap
-      text-overflow ellipsis
 </style>
